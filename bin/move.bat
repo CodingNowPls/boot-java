3f314ffc321629c84e3ef7c2e78b2a316ed30d31
gao <903092391@qq.com>
2025-05-07 14:17:40 +0800

    添加拷贝脚本

--- bin/move.bat
+++ bin/move.bat
@@ -8,12 +8,10 @@ set DIST_DIR=E:\workspace\xxx\boot-ui\dist
 
 :: ========== 操作执行 ==========
 :: 删除旧文件
-del /f /q "%SERVER_RESOURCES%\templates\index.html"
-rd /s /q "%SERVER_RESOURCES%\static\assets"
+rd /s /q "%SERVER_RESOURCES%\static"
 
 :: 复制新文件
-copy /y "%DIST_DIR%\index.html" "%SERVER_RESOURCES%\templates\"
-xcopy /e /y "%DIST_DIR%\assets\*" "%SERVER_RESOURCES%\static\assets\"
+xcopy /e /y "%DIST_DIR%\*" "%SERVER_RESOURCES%\static\"
 
 echo 操作已完成
 pause
