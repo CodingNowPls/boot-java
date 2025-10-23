6813ec38251d8df771b276df36c2291946844699
gao <903092391@qq.com>
2025-10-23 15:48:06 +0800

    先注释

--- boot-common/boot-common-jimu-report/src/main/java/com/boot/jimu/report/JimuReportTokenService.java
+++ boot-common/boot-common-jimu-report/src/main/java/com/boot/jimu/report/JimuReportTokenService.java
@@ -1,114 +1,114 @@
-package com.boot.jimu.report;
-
-import cn.dev33.satoken.exception.NotLoginException;
-import com.boot.common.core.domain.entity.SysRole;
-import com.boot.common.core.utils.ServletUtils;
-import com.boot.common.core.utils.StringUtils;
-import com.boot.common.core.utils.json.BootJsonUtil;
-import com.boot.common.security.core.domain.model.LoginUser;
-import com.boot.common.security.service.TokenService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import org.jeecg.modules.jmreport.api.JmReportTokenServiceI;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
-
-@Component
-@AllArgsConstructor
-public class JimuReportTokenService implements JmReportTokenServiceI {
-
-    @Autowired
-    private TokenService tokenService;
-    @Override
-    public HttpHeaders customApiHeader() {
-
-        HttpHeaders header = new HttpHeaders();
-        //主要用于API数据源。默认给API数据源的header中携带上Token。
-        //如使用当前项目的API，则需要在header中携带Authorization头
-        header.add(tokenService.getHeader(), getToken());
-        header.add("token", getToken());
-        header.add("X-Access-Token", getToken());
-        return header;
-    }
-    @Override
-    public String getUsername(String token) {
-        try {
-            LoginUser loginUser = tokenService.getLoginUserFromToken(token);
-            if (Objects.isNull(loginUser)){
-                return null;
-            }
-            return loginUser.getUserName();
-        } catch (Exception e) {
-            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
-        }
-    }
-
-    @Override
-    public Boolean verifyToken(String token) {
-        try {
-            tokenService.verifyToken(token);
-            return true;
-        } catch (Exception e) {
-            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
-        }
-    }
-
-    @Override
-    public String getToken(HttpServletRequest request) {
-        try {
-            String token = tokenService.getToken(request);
-            return token;
-        } catch (Exception e) {
-           e.printStackTrace();
-        }
-        throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
-    }
-
-    @Override
-    public Map<String, Object> getUserInfo(String token) {
-        LoginUser loginUser = tokenService.getLoginUserFromToken(token);
-        if (Objects.isNull(loginUser)) {
-            return null;
-        }
-        return BootJsonUtil.toMap(loginUser);
-    }
-
-
-    @Override
-    public String[] getRoles(String token) {
-        //后期要设置角色 https://help.jimureport.com/prodSafe
-        //拥有角色admin、dbadeveloper 、lowdeveloper 用户可以访问敏感接口。 拥有角色admin、lowdeveloper 用户在关闭在线报表设计情况下，也可以设计报表
-        //admin	超级管理员	拥有最高权限
-        //dbadeveloper	DB管理员	可以设置数据库连接
-        //lowdeveloper	报表设计管理员	可以设计报表
-        LoginUser loginUser = tokenService.getLoginUserFromToken(token);
-        if (Objects.isNull(loginUser)) {
-            return new String[]{};
-        }
-        List<SysRole> roles = loginUser.getUser().getRoles();
-        String[] userRoles = new String[roles.size()];
-        for (SysRole role : roles) {
-            userRoles[roles.indexOf(role)] = role.getRoleKey();
-        }
-        //new String[]{"admin", "lowdeveloper", "dbadeveloper"}
-        return userRoles;
-    }
-
-    @Override
-    public String[] getPermissions(String token) {
-        //drag:datasource:testConnection	仪表盘数据库连接测试
-        //onl:drag:clear:recovery	仪表盘清空回收站
-        //drag:analysis:sql	仪表盘SQL解析
-        //drag:design:getTotalData	仪表盘对Online表单展示数据
-        //onl:drag:page:delete 仪表盘数据删除
-        return new String[]{"drag:datasource:testConnection", "onl:drag:clear:recovery", "drag:analysis:sql", "drag:design:getTotalData", "onl:drag:page:delete"};
-    }
-}
+//package com.boot.jimu.report;
+//
+//import cn.dev33.satoken.exception.NotLoginException;
+//import com.boot.common.core.domain.entity.SysRole;
+//import com.boot.common.core.utils.ServletUtils;
+//import com.boot.common.core.utils.StringUtils;
+//import com.boot.common.core.utils.json.BootJsonUtil;
+//import com.boot.common.security.core.domain.model.LoginUser;
+//import com.boot.common.security.service.TokenService;
+//import jakarta.servlet.http.Cookie;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.AllArgsConstructor;
+//import org.jeecg.modules.jmreport.api.JmReportTokenServiceI;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Objects;
+//
+//
+//@Component
+//@AllArgsConstructor
+//public class JimuReportTokenService implements JmReportTokenServiceI {
+//
+//    @Autowired
+//    private TokenService tokenService;
+//    @Override
+//    public HttpHeaders customApiHeader() {
+//
+//        HttpHeaders header = new HttpHeaders();
+//        //主要用于API数据源。默认给API数据源的header中携带上Token。
+//        //如使用当前项目的API，则需要在header中携带Authorization头
+//        header.add(tokenService.getHeader(), getToken());
+//        header.add("token", getToken());
+//        header.add("X-Access-Token", getToken());
+//        return header;
+//    }
+//    @Override
+//    public String getUsername(String token) {
+//        try {
+//            LoginUser loginUser = tokenService.getLoginUserFromToken(token);
+//            if (Objects.isNull(loginUser)){
+//                return null;
+//            }
+//            return loginUser.getUserName();
+//        } catch (Exception e) {
+//            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
+//        }
+//    }
+//
+//    @Override
+//    public Boolean verifyToken(String token) {
+//        try {
+//            tokenService.verifyToken(token);
+//            return true;
+//        } catch (Exception e) {
+//            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
+//        }
+//    }
+//
+//    @Override
+//    public String getToken(HttpServletRequest request) {
+//        try {
+//            String token = tokenService.getToken(request);
+//            return token;
+//        } catch (Exception e) {
+//           e.printStackTrace();
+//        }
+//        throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
+//    }
+//
+//    @Override
+//    public Map<String, Object> getUserInfo(String token) {
+//        LoginUser loginUser = tokenService.getLoginUserFromToken(token);
+//        if (Objects.isNull(loginUser)) {
+//            return null;
+//        }
+//        return BootJsonUtil.toMap(loginUser);
+//    }
+//
+//
+//    @Override
+//    public String[] getRoles(String token) {
+//        //后期要设置角色 https://help.jimureport.com/prodSafe
+//        //拥有角色admin、dbadeveloper 、lowdeveloper 用户可以访问敏感接口。 拥有角色admin、lowdeveloper 用户在关闭在线报表设计情况下，也可以设计报表
+//        //admin	超级管理员	拥有最高权限
+//        //dbadeveloper	DB管理员	可以设置数据库连接
+//        //lowdeveloper	报表设计管理员	可以设计报表
+//        LoginUser loginUser = tokenService.getLoginUserFromToken(token);
+//        if (Objects.isNull(loginUser)) {
+//            return new String[]{};
+//        }
+//        List<SysRole> roles = loginUser.getUser().getRoles();
+//        String[] userRoles = new String[roles.size()];
+//        for (SysRole role : roles) {
+//            userRoles[roles.indexOf(role)] = role.getRoleKey();
+//        }
+//        //new String[]{"admin", "lowdeveloper", "dbadeveloper"}
+//        return userRoles;
+//    }
+//
+//    @Override
+//    public String[] getPermissions(String token) {
+//        //drag:datasource:testConnection	仪表盘数据库连接测试
+//        //onl:drag:clear:recovery	仪表盘清空回收站
+//        //drag:analysis:sql	仪表盘SQL解析
+//        //drag:design:getTotalData	仪表盘对Online表单展示数据
+//        //onl:drag:page:delete 仪表盘数据删除
+//        return new String[]{"drag:datasource:testConnection", "onl:drag:clear:recovery", "drag:analysis:sql", "drag:design:getTotalData", "onl:drag:page:delete"};
+//    }
+//}
--- boot-common/boot-common-magic-api/src/main/java/com/boot/common/magicapi/interceptor/MagicLoginAuthorizationInterceptor.java
+++ boot-common/boot-common-magic-api/src/main/java/com/boot/common/magicapi/interceptor/MagicLoginAuthorizationInterceptor.java
@@ -1,123 +1,123 @@
-package com.boot.common.magicapi.interceptor;
-
-import cn.dev33.satoken.exception.NotLoginException;
-import com.boot.common.core.cache.BootCache;
-import com.boot.common.core.constant.Constants;
-import com.boot.common.core.utils.MessageUtils;
-import com.boot.common.core.utils.ServletUtils;
-import com.boot.common.core.utils.sign.RsaUtils;
-import com.boot.common.log.manager.AsyncManager;
-import com.boot.common.log.manager.factory.AsyncFactory;
-import com.boot.common.security.core.domain.model.LoginUser;
-import com.boot.common.security.service.PermissionService;
-import com.boot.common.security.service.SysLoginService;
-import com.boot.common.security.service.TokenService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.ssssssss.magicapi.core.context.MagicUser;
-import org.ssssssss.magicapi.core.exception.MagicLoginException;
-import org.ssssssss.magicapi.core.interceptor.AuthorizationInterceptor;
-
-import java.util.Objects;
-
-@Component
-public class MagicLoginAuthorizationInterceptor implements AuthorizationInterceptor {
-
-    // 令牌秘钥
-    @Value("${token.secret}")
-    private String secret;
-
-    // 令牌有效期（默认30分钟）
-    @Value("${token.expireTime}")
-    private int expireTime;
-
-    @Autowired
-    private SysLoginService sysLoginService;
-
-    @Autowired
-    private BootCache bootCache;
-
-    @Value("${magic-api.needLogin}")
-    private Boolean needLogin;
-
-    @Autowired
-    private TokenService tokenService;
-
-    @Autowired
-    private PermissionService permissionService;
-
-    /**
-     * 配置是否需要登录
-     */
-    @Override
-    public boolean requireLogin() {
-        return needLogin;
-    }
-
-    /**
-     * 根据Token获取User
-     */
-    @Override
-    public MagicUser getUserByToken(String token) {
-        try {
-            LoginUser user = tokenService.getLoginUserFromToken(token);
-            if (Objects.isNull(user)) {
-                throw new MagicLoginException("请从网站登录页登陆");
-            }
-            HttpServletRequest request = ServletUtils.getRequest();
-            request.setAttribute("token", token);
-            return new MagicUser(
-                    user.getUserId().toString(),
-                    user.getUserName(),
-                    token,
-                    expireTime
-            );
-        } catch (Exception e) {
-            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
-        }
-    }
-
-    @Override
-    public MagicUser login(String username, String password) {
-        try {
-            String token;
-            try {
-                String encryptPassword = RsaUtils.encryptByPublicKey(RsaUtils.publicKey, password);
-                token = sysLoginService.login(username, encryptPassword, SysLoginService.NOT_NEED_CHECK_CODE, null);
-            } catch (Exception e) {
-                throw new MagicLoginException("登陆失败");
-            }
-            //需要校验权限
-            boolean permi = permissionService.hasPermi("magic:api:code", token);
-            if (!permi) {
-                throw new MagicLoginException("无权限");
-            }
-            MagicUser user = getUserByToken(token);
-            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_SUCCESS, MessageUtils.message("user.login.success")));
-            // 返回登录信息  登录成功后将用户信息封装成magic-api的用户信息
-            return new MagicUser(user.getId().toString(), username, token, expireTime);
-        } catch (Exception e) {
-            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
-        }
-    }
-
-    @Override
-    public void refreshToken(MagicUser user) {
-        try {
-            String token = user.getToken();
-            tokenService.verifyToken(token);
-        } catch (Exception e) {
-            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
-        }
-    }
-
-    @Override
-    public void logout(String token) {
-       //tokenService.loginOut();
-    }
-}
-
+//package com.boot.common.magicapi.interceptor;
+//
+//import cn.dev33.satoken.exception.NotLoginException;
+//import com.boot.common.core.cache.BootCache;
+//import com.boot.common.core.constant.Constants;
+//import com.boot.common.core.utils.MessageUtils;
+//import com.boot.common.core.utils.ServletUtils;
+//import com.boot.common.core.utils.sign.RsaUtils;
+//import com.boot.common.log.manager.AsyncManager;
+//import com.boot.common.log.manager.factory.AsyncFactory;
+//import com.boot.common.security.core.domain.model.LoginUser;
+//import com.boot.common.security.service.PermissionService;
+//import com.boot.common.security.service.SysLoginService;
+//import com.boot.common.security.service.TokenService;
+//import jakarta.servlet.http.Cookie;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//import org.ssssssss.magicapi.core.context.MagicUser;
+//import org.ssssssss.magicapi.core.exception.MagicLoginException;
+//import org.ssssssss.magicapi.core.interceptor.AuthorizationInterceptor;
+//
+//import java.util.Objects;
+//
+//@Component
+//public class MagicLoginAuthorizationInterceptor implements AuthorizationInterceptor {
+//
+//    // 令牌秘钥
+//    @Value("${token.secret}")
+//    private String secret;
+//
+//    // 令牌有效期（默认30分钟）
+//    @Value("${token.expireTime}")
+//    private int expireTime;
+//
+//    @Autowired
+//    private SysLoginService sysLoginService;
+//
+//    @Autowired
+//    private BootCache bootCache;
+//
+//    @Value("${magic-api.needLogin}")
+//    private Boolean needLogin;
+//
+//    @Autowired
+//    private TokenService tokenService;
+//
+//    @Autowired
+//    private PermissionService permissionService;
+//
+//    /**
+//     * 配置是否需要登录
+//     */
+//    @Override
+//    public boolean requireLogin() {
+//        return needLogin;
+//    }
+//
+//    /**
+//     * 根据Token获取User
+//     */
+//    @Override
+//    public MagicUser getUserByToken(String token) {
+//        try {
+//            LoginUser user = tokenService.getLoginUserFromToken(token);
+//            if (Objects.isNull(user)) {
+//                throw new MagicLoginException("请从网站登录页登陆");
+//            }
+//            HttpServletRequest request = ServletUtils.getRequest();
+//            request.setAttribute("token", token);
+//            return new MagicUser(
+//                    user.getUserId().toString(),
+//                    user.getUserName(),
+//                    token,
+//                    expireTime
+//            );
+//        } catch (Exception e) {
+//            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
+//        }
+//    }
+//
+//    @Override
+//    public MagicUser login(String username, String password) {
+//        try {
+//            String token;
+//            try {
+//                String encryptPassword = RsaUtils.encryptByPublicKey(RsaUtils.publicKey, password);
+//                token = sysLoginService.login(username, encryptPassword, SysLoginService.NOT_NEED_CHECK_CODE, null);
+//            } catch (Exception e) {
+//                throw new MagicLoginException("登陆失败");
+//            }
+//            //需要校验权限
+//            boolean permi = permissionService.hasPermi("magic:api:code", token);
+//            if (!permi) {
+//                throw new MagicLoginException("无权限");
+//            }
+//            MagicUser user = getUserByToken(token);
+//            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_SUCCESS, MessageUtils.message("user.login.success")));
+//            // 返回登录信息  登录成功后将用户信息封装成magic-api的用户信息
+//            return new MagicUser(user.getId().toString(), username, token, expireTime);
+//        } catch (Exception e) {
+//            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
+//        }
+//    }
+//
+//    @Override
+//    public void refreshToken(MagicUser user) {
+//        try {
+//            String token = user.getToken();
+//            tokenService.verifyToken(token);
+//        } catch (Exception e) {
+//            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
+//        }
+//    }
+//
+//    @Override
+//    public void logout(String token) {
+//       //tokenService.loginOut();
+//    }
+//}
+//
