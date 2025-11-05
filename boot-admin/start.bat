10a452b843d4b5e3e944dd0c42eb7a3cb4d712ad
gao <903092391@qq.com>
2025-11-05 14:18:20 +0800

    修复bug

--- boot-admin/start.bat
+++ boot-admin/start.bat
@@ -4,6 +4,6 @@ title boot-java
 set JAVA_HOME=D:\program\Java\jdk\jdk-21.0.2
 set PATH=%JAVA_HOME%\bin;%PATH%
 
-java -Xms128m -Xmx256m -cp "boot-admin.jar;lib/*" -Dspring.config.location=application.yml,application-dev.yml com.boot.BootApplication
+java -Xms128m -Xmx256m -cp "boot-admin.jar;lib/*" -Dspring.config.location=application.yml com.boot.BootApplication
 
 pause
--- boot-admin/start.sh
+++ boot-admin/start.sh
@@ -6,7 +6,7 @@ export JAVA_HOME=/path/to/your/jdk-21.0.2
 export PATH=$JAVA_HOME/bin:$PATH
 
 # 运行Java应用程序
-java -Xms128m -Xmx256m -cp "boot-admin.jar:lib/*" -Dspring.config.location=application.yml,application-dev.yml com.boot.BootApplication
+java -Xms128m -Xmx256m -cp "boot-admin.jar:lib/*" -Dspring.config.location=application.yml com.boot.BootApplication
 
 # 暂停脚本执行（可选）
 read -p "Press any key to continue..."
