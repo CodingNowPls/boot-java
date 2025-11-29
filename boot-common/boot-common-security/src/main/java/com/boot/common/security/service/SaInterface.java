6e4e43d7c1abe8252b0a789d581cd0f98f0328db
gao <903092391@qq.com>
2025-11-29 12:32:54 +0800

    修复bug

--- boot-common/boot-common-security/src/main/java/com/boot/common/security/service/SaInterface.java
+++ boot-common/boot-common-security/src/main/java/com/boot/common/security/service/SaInterface.java
@@ -1,11 +1,13 @@
 package com.boot.common.security.service;
 
+import cn.dev33.satoken.exception.NotLoginException;
 import cn.dev33.satoken.stp.StpInterface;
 import com.boot.common.security.core.domain.model.LoginUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 
@@ -22,6 +24,9 @@ public class SaInterface implements StpInterface {
     @Override
     public List<String> getPermissionList(Object o, String s) {
         LoginUser loginUser = tokenService.getLoginUser();
+        if (Objects.isNull(loginUser)){
+            throw new NotLoginException(NotLoginException.TOKEN_TIMEOUT_MESSAGE, "", NotLoginException.TOKEN_TIMEOUT);
+        }
         //采用的是用户里自带的权限，实现一次性访问reids,进行判断是否可以访问
         return loginUser.getPermissions().stream().collect(Collectors.toList());
     }
